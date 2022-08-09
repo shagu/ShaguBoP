0aa413780b8e2c3e3ed88b195b4fd2e284f158b9
shagu <ericmauser@googlemail.com>
2022-08-09 20:05:16 +0200

    ShaguBoP: auto loot after confirmation dialog

--- ShaguBoP.lua
+++ ShaguBoP.lua
@@ -1,13 +1,16 @@
-ShaguBoP = CreateFrame("Frame", nil, nil)  
+ShaguBoP = CreateFrame("Frame", nil, nil)
 ShaguBoP:RegisterEvent("LOOT_BIND_CONFIRM")
-
+ShaguBoP:RegisterEvent("LOOT_OPENED")
 ShaguBoP:SetScript("OnEvent", function()
   -- auto accept BoP loot in solo mode
   if GetNumPartyMembers() == 0 and GetNumRaidMembers() == 0 then
-    local dialog = StaticPopup_Show("LOOT_BIND")
-    if dialog then
-      dialog.data = arg1
+    if event == "LOOT_BIND_CONFIRM" then
+      LootSlot(arg1)
       StaticPopup1Button1:Click()
+    elseif event == "LOOT_OPENED" then
+      for i=1,GetNumLootItems() do
+        LootSlot(i)
+      end
     end
   end
 end)
